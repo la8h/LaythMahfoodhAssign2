f60139a8833c21d3b320d7cfccf5f43ed4c35792
mahfoodh1 <mahfoodh@sheridancollege.ca>
2021-03-02 11:35:47 -0500

    Add overflow menu and three radio buttons to main activity.

--- LaythAssign22/src/main/java/layth/mahfoodh/s991523983/LaythActivity.java
+++ LaythAssign22/src/main/java/layth/mahfoodh/s991523983/LaythActivity.java
@@ -6,7 +6,13 @@ package layth.mahfoodh.s991523983;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class LaythActivity extends AppCompatActivity {
 
@@ -14,5 +20,54 @@ public class LaythActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.overview_menu, menu);
+        return true;
+    }
+    //
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = null;
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.help:
+             //   Toast.makeText(this, "You selected start!", Toast.LENGTH_LONG).show();
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.doordash.com/en-CA"));
+                startActivity(intent);
+                break;
+
+            case R.id.icon:
+              //  Toast.makeText(this, "You selected Play!", Toast.LENGTH_LONG).show();
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.dominos.ca/"));
+                startActivity(intent);
+                break;
+
+            case R.id.name:
+                Toast.makeText(this, "This is a pizza restaurants app!", Toast.LENGTH_LONG).show();
+                break;
+/*
+            case R.id.stop:
+                Toast.makeText(this, "You selected stop!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.help:
+                Toast.makeText(this, "You selected help!", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nothelp:
+                Toast.makeText(this, "You selected nothelp!", Toast.LENGTH_LONG).show();
+                break;
+
+ */
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+        return true;
+
     }
 }
