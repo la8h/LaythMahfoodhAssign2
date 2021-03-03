caea37e3a9f18e07fe34c811e23321b1e4773788
mahfoodh1 <mahfoodh@sheridancollege.ca>
2021-03-02 22:09:49 -0500

    Add radio button and checkboxes to the second Activity.

---
+++ LaythAssign22/src/main/java/layth/mahfoodh/s991523983/MahfoodhActivity3.java
@@ -0,0 +1,18 @@
+/*
+Layth Mahfoodh s991523983 PROG38448
+ */
+
+package layth.mahfoodh.s991523983;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MahfoodhActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mahfoodh3);
+    }
+}
